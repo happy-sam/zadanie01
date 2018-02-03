9f6767681f228553ded0c7bc390d7bc5bacb6797
Stanisław Romek <info@happy.ovh>
2018-02-03 22:26:46 +0100

    Poprawka ostylowania

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@ var c = prompt("Podaj trzecią liczbę");
 var value = (2 * a * b) + (3 * b * c) - (4 * b * c);
  
 if (value > 0) {
-    document.write("zmienna a = " + a + "<br>");
-    document.write("zmienna b = " + b + "<br>");
-    document.write("zmienna c = " + c + "<br>");
+    document.write("<p>" + "zmienna a = " + a + "</p>");
+    document.write("<p>" + "zmienna b = " + b + "</p>");
+    document.write("<p>" + "zmienna c = " + c + "</p>" + "<br>");
     document.write("<span class='wartosc'>" + "Wartość jest dodatnia i wynosi " + value + "</span>");
 } else if (value < 0) {
-    document.write("zmienna a = " + a + "<br>");
-    document.write("zmienna b = " + b + "<br>");
-    document.write("zmienna c = " + c + "<br>");
+    document.write("<p>" + "zmienna a = " + a + "</p>");
+    document.write("<p>" + "zmienna b = " + b + "</p>");
+    document.write("<p>" + "zmienna c = " + c + "</p>" + "<br>");
     document.write("<span class='wartosc'>" + "Wartość jest ujemna i wynosi " + value + "</span>");
 } else {
-    document.write("zmienna a = " + a + "<br>");
-    document.write("zmienna b = " + b + "<br>");
-    document.write("zmienna c = " + c + "<br>");
+    document.write("<p>" + "zmienna a = " + a + "</p>");
+    document.write("<p>" + "zmienna b = " + b + "</p>");
+    document.write("<p>" + "zmienna c = " + c + "</p>" + "<br>");
     document.write("<span class='wartosc'>" + "Wartość wynosi " + value + "</span>");
 }
 
